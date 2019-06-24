ed26b0dae88b9617fb4a2b165126c3c51f9a597c
Kushagra Gupta <kushgpt08@gmail.com>
2019-06-24 14:48:05 +0530

    apps made

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,2 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    upload_date = models.DateField()
+    title = models.TextField(max_length=255)
+    image = models.ImageField()
 
