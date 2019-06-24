f77139c3e788d32528cb492960014681b477267f
Namannimmo <namangera15@gmail.com>
2019-06-24 15:41:53 +0530

    post-models change

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    upload_date = models.DateField()
+    upload_date = models.DateField(auto_now_add=True)
     title = models.CharField(max_length=255)
     image = models.ImageField()
 
