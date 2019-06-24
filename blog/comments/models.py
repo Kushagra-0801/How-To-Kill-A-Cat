dbead5424efc1085bf0bdc4c1d403bb8e83bdf6c
Namannimmo <namangera15@gmail.com>
2019-06-24 15:15:48 +0530

    comment-model

--- blog/comments/models.py
+++ blog/comments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    upload_date = models.DateField()
+    upvotes = models.IntegerField()
+    downvotes = models.IntegerField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
+
