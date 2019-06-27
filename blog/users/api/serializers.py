386b52d442ce957c20887fda181891a2bd4d7855
Kushagra Gupta <kushgpt08@gmail.com>
2019-06-27 13:20:27 +0530

    user login api endpoint

--- blog/users/api/serializers.py
+++ blog/users/api/serializers.py
@@ -1,17 +1,22 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+
     def create(self, validated_data):
         user = User.objects.create_user(
-            username = validated_data["username"],
-            email = validated_data["email"],
-            password = validated_data["password"]
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+            location=validated_data["location"],
         )
         return user
-    
+
     class Meta:
         model = User
-        fields = ["id","username","email","password"]
+        fields = ["id", "username", "email", "password", "location"]
+
--- blog/users/api/urls.py
+++ blog/users/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import UserCreateApiView
-
+from rest_framework_jwt.views import obtain_jwt_token
 
 
 urlpatterns = [
-    path("user/register/",UserCreateApiView.as_view(), name = "create-user-api-view")
+    path("user/register/", UserCreateApiView.as_view(), name="create-user-api-view"),
+    path("auth/login/", obtain_jwt_token, name="api-login"),
 ]
